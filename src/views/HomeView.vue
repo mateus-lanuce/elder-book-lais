d2348be37e2145dc15cfc79f05aa8db8a4a31fd4
mateus-lanuce <mateuslanuce@gmail.com>
2022-08-05 18:56:35 -0300

    feat: :sparkles: add tailwind css and delete some vue pattern archives

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <p class="text-sky-400">abacate</p>
 </template>
